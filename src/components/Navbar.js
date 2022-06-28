2b2ef78da4bb4067f4ffddfc553fd3c6877eefdf
DuanLeeWatkins <leeduan19@gmail.com>
2022-06-28 18:59:18 -0400

    updated navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,35 +7,28 @@ import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 
 
 
 export default function Navbar() {
     const pages = ['About', 'Pricing', 'Contact us'];
-    const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+    
 
     const ResponsiveAppBar = () => {
         const [anchorElNav, setAnchorElNav] = React.useState(null);
-        const [anchorElUser, setAnchorElUser] = React.useState(null);
 
         const handleOpenNavMenu = (event) => {
             setAnchorElNav(event.currentTarget);
         };
-        const handleOpenUserMenu = (event) => {
-            setAnchorElUser(event.currentTarget);
-        };
+        
 
         const handleCloseNavMenu = () => {
             setAnchorElNav(null);
         };
 
-        const handleCloseUserMenu = () => {
-            setAnchorElUser(null);
-        };
+        
 
         return (
             <AppBar position="static">
@@ -50,7 +43,7 @@ export default function Navbar() {
                             sx={{
                                 mr: 2,
                                 display: { xs: 'none', md: 'flex' },
-                                fontFamily: 'monospace',
+                                fontFamily: 'Roboto',
                                 fontWeight: 700,
                                 letterSpacing: '.3rem',
                                 color: 'inherit',
@@ -102,10 +95,10 @@ export default function Navbar() {
                             component="a"
                             href=""
                             sx={{
-                                mr: 2,
+                                mr: 1,
                                 display: { xs: 'flex', md: 'none' },
                                 flexGrow: 1,
-                                fontFamily: 'monospace',
+                                fontFamily: 'Roboto',
                                 fontWeight: 700,
                                 letterSpacing: '.3rem',
                                 color: 'inherit',
@@ -125,36 +118,10 @@ export default function Navbar() {
                                 </Button>
                             ))}
                         </Box>
+                        <Button color="inherit">Login</Button>
+                        <Button color="inherit">Sign up</Button>
 
-                        <Box sx={{ flexGrow: 0 }}>
-                            <Tooltip title="Open settings">
-                                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                                    <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-                                </IconButton>
-                            </Tooltip>
-                            <Menu
-                                sx={{ mt: '45px' }}
-                                id="menu-appbar"
-                                anchorEl={anchorElUser}
-                                anchorOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'right',
-                                }}
-                                keepMounted
-                                transformOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'right',
-                                }}
-                                open={Boolean(anchorElUser)}
-                                onClose={handleCloseUserMenu}
-                            >
-                                {settings.map((setting) => (
-                                    <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                                        <Typography textAlign="center">{setting}</Typography>
-                                    </MenuItem>
-                                ))}
-                            </Menu>
-                        </Box>
+                        
                     </Toolbar>
                 </Container>
             </AppBar>
